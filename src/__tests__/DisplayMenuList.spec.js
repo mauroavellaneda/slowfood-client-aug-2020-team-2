0799e6cff5d8e0d19eca42601dae6e016adc6ce5
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-09-23 18:01:58 +0200

    Restarting the MenuList component, install axios, starting unit tests for unit spec for MenuList component.

--- src/__tests__/DisplayMenuList.spec.js
+++ src/__tests__/DisplayMenuList.spec.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { shallow } from "enzyme";
-
 import MenuList from "../components/MenuList";
+import axios from "axios";
 
 describe("<MenuList />", () => {
-	let describedComponent;
-	describe("displays the menu", () => {
-		beforeAll(() => {
-			describedComponent = shallow(
-      )
-    })
-  })
+	it('should fetch MenuList from api using Axios', () => {
+		const axiosSpy = jest.spyOn(axios, 'get')
+		shallow(<MenuList/>)
+		expect(axiosSpy).toBeCalled()
+	});
 })
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
+let apiUrl;
+if (process.env.NODE_ENV === "production") {
+  apiUrl = "https://ca-cooper-api.herokuapp.com/api/v1";
+} else {
+  apiUrl = "http://localhost:3000/api/v1";
+}
+axios.defaults.baseURL = apiUrl;
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
