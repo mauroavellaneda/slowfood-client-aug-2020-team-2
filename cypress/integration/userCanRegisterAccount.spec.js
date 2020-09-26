d2ef3643275fbe7a38a452f14ad64da234f9f3aa
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2020-09-26 09:46:39 +0200

    Adding feature test for 'user can register an account'

---
+++ cypress/integration/userCanRegisterAccount.spec.js
@@ -0,0 +1,29 @@
+describe("User sing up", ()=> {
+    beforeEach(() => {
+        cy.visit("/");
+
+    });
+    it("successfully sign up with valid credentials", () => {
+        cy.get("#signup").click();
+        cy.get("#signup-form").within(() => {
+            cy.get("#email").type("user@mail.com");
+            cy.get("#password").type("password");
+            cy.get("#confirm-password").type("password");
+            cy.get('button').contains('Submit').click()
+        });
+        cy.get("#message").should("contain", "user@mail.com has been successfully registered");
+    });
+    it("unsuccessfully sign up with invalid credentials", () => {
+        cy.get("#signup").click();
+        cy.get("#signup-form").within(() => {
+            cy.get("#email").type("user@mail.com");
+            cy.get("#password").type("wrongpassword");
+            cy.get("#confirm-password").type("wrongpassword");
+            cy.get('button').contains('Submit').click()
+        });
+        cy.get("#message").should("contain", "Account has been not registered. Try again");
+
+    });
+
+
+})
