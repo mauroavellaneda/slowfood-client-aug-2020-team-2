7a5e307e43fa667e84524af1441e579fee6c2257
emiliano-ma <emilianomainero@gmail.com>
2020-09-27 13:46:47 +0200

    Finish refactoring. Removing component, form and spec for signup. Login spec going green

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,35 +1,22 @@
 import axios from "axios";
 
-// const authenticate = async (email, password, confirm_password ) => {
-//   try {
-//     const response = await axios.post("/auth/", {
-//       email: email,
-//       password: password,
-//       confirm_password: confirm_password
-//     });
-//     await storeAuthCredentials(response);
-//     return { authenticated: true };
-//   } catch (error) {
-//     return { authenticated: false, message: error.response.data.errors[0] };
-//   }
-// };
-
 const authLogin = async (email, password) => {
-  let authResponse
+  let authResponse;
   try {
     const response = await axios.post("/auth/sign_in", {
       email: email,
       password: password,
     });
-    await storeAuthCredentials(response);
-    authResponse = { data: response.data.data, authenticated: true };
+    storeAuthCredentials(response);
+    return (authResponse = { data: response.data.data, authenticated: true });
   } catch (error) {
-    authResponse = {
+    return (authResponse = {
       authenticated: false,
       data: error.response.data.errors[0],
-    };
+    });
   }
 };
+
 const storeAuthCredentials = ({ headers }) => {
   const credentials = {
     uid: headers["uid"],
